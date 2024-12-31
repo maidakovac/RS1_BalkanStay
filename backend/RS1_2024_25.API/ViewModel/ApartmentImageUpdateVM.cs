b66e96c8cc5613fe75ed7025ebda2bb5219b2fb7
Maida Kovac <maidakvc@gmail.com>
2024-12-31 14:36:43 +0100

    48 CRUD ApartmentImage

---
+++ backend/RS1_2024_25.API/ViewModel/ApartmentImageUpdateVM.cs
@@ -0,0 +1,12 @@
+﻿using RS1_2024_25.API.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.ViewModel
+{
+    public class ApartmentImageUpdateVM
+    {
+        public int ApartmentImageID { get; set; }
+        public int ApartmentId { get; set; }
+        public int ImageID { get; set; }
+    }
+}
