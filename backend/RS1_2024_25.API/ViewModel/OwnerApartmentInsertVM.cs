8f152e2ea1ffdbd13f815e948d78bd92bb8423be
Maida Kovac <maidakvc@gmail.com>
2024-12-31 14:10:16 +0100

    46 CRUD OwnerApartment

---
+++ backend/RS1_2024_25.API/ViewModel/OwnerApartmentInsertVM.cs
@@ -0,0 +1,8 @@
+﻿namespace RS1_2024_25.API.ViewModel
+{
+    public class OwnerApartmentInsertVM
+    {
+        public int OwnerID { get; set; }
+        public int ApartmentId { get; set; }
+    }
+}
