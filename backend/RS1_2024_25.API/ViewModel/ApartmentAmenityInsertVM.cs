8c83c03c9188e6a20668b7484169fb2970d791f0
Maida Kovac <maidakvc@gmail.com>
2024-12-31 15:22:26 +0100

    52 CRUD ApartmentAmenity

---
+++ backend/RS1_2024_25.API/ViewModel/ApartmentAmenityInsertVM.cs
@@ -0,0 +1,8 @@
+﻿namespace RS1_2024_25.API.ViewModel
+{
+    public class ApartmentAmenityInsertVM
+    {
+        public int ApartmentId { get; set; }
+        public int AmenityID { get; set; }
+    }
+}
---
+++ backend/RS1_2024_25.API/ViewModel/ApartmentAmenityUpdateVM.cs
@@ -0,0 +1,12 @@
+﻿using RS1_2024_25.API.Data;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RS1_2024_25.API.ViewModel
+{
+    public class ApartmentAmenityUpdateVM
+    {
+        public int ApartmentAmenityID { get; set; }
+        public int ApartmentId { get; set; }
+        public int AmenityID { get; set; }
+    }
+}
