8424d65ff89f248e88e52e0b44217887cf21c634
Maida Kovac <maidakvc@gmail.com>
2025-03-05 19:13:12 +0100

    104 Mail validation

--- frontend/RS1_2024_25_angular_template/src/app/modules/public/contact-us/contact-us.component.ts
+++ frontend/RS1_2024_25_angular_template/src/app/modules/public/contact-us/contact-us.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,6 +6,30 @@ import {Component} from '@angular/core';
   styleUrl: './contact-us.component.css',
   standalone: false
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const form = document.getElementById('contactForm') as HTMLFormElement;
+    const emailInput = document.getElementById('mail') as HTMLInputElement;
+    const errorMessage = document.createElement('small');
 
+    errorMessage.style.color = 'red';
+    emailInput.parentNode?.insertBefore(errorMessage, emailInput.nextSibling);
+
+    emailInput.addEventListener('input', () => {
+      const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      if (!emailPattern.test(emailInput.value)) {
+        errorMessage.textContent = '';
+      } else {
+        errorMessage.textContent = '';
+      }
+    });
+
+    form.addEventListener('submit', (event) => {
+      if (!emailInput.value.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+        event.preventDefault(); // Sprjecava slanje forme ako e-mail nije validan
+        errorMessage.textContent = 'Please enter a valid email address.';
+        alert('Invalid email! Please enter a valid email address.');
+      }
+    });
+  }
 }
