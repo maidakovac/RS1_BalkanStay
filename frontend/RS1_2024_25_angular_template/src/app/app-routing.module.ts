07f077ec1fb6000b55b5ec53dec388af25798f6e
izelrepuh <izel.repuh@edu.fit.ba>
2025-03-14 22:44:52 +0100

    109 Login forma sa autentifikacijom

--- frontend/RS1_2024_25_angular_template/src/app/app-routing.module.ts
+++ frontend/RS1_2024_25_angular_template/src/app/app-routing.module.ts
@@ -1,34 +1,33 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {UnauthorizedComponent} from './modules/shared/unauthorized/unauthorized.component';
-import {AuthGuard} from './auth-guards/auth-guard.service';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UnauthorizedComponent } from './modules/shared/unauthorized/unauthorized.component';
+import { AuthGuard } from './auth-guards/auth-guard.service';
 
 const routes: Routes = [
-  {path: 'unauthorized', component: UnauthorizedComponent},
+  { path: 'unauthorized', component: UnauthorizedComponent },
   {
     path: 'admin',
     canActivate: [AuthGuard],
-    data: {isAdmin: true}, // Proslijeđivanje potrebnih prava pristupa, ako je potrebno
-    loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule)  // Lazy load  modula
+    data: { isAdmin: true },
+    loadChildren: () => import('./modules/admin/admin.module').then(m => m.AdminModule)
   },
   {
     path: 'public',
-    loadChildren: () => import('./modules/public/public.module').then(m => m.PublicModule)  // Lazy load  modula
+    loadChildren: () => import('./modules/public/public.module').then(m => m.PublicModule)
   },
   {
     path: 'client',
-    loadChildren: () => import('./modules/client/client.module').then(m => m.ClientModule)  // Lazy load  modula
+    loadChildren: () => import('./modules/client/client.module').then(m => m.ClientModule)
   },
   {
     path: 'auth',
-    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)  // Lazy load  modula
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
   },
-  {path: '**', redirectTo: 'public', pathMatch: 'full'}  // Default ruta koja vodi na public
+  { path: '**', redirectTo: 'public', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
--- frontend/RS1_2024_25_angular_template/src/app/app.module.ts
+++ frontend/RS1_2024_25_angular_template/src/app/app.module.ts
@@ -1,14 +1,13 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {MyAuthInterceptor} from './services/auth-services/my-auth-interceptor.service';
-import {MyAuthService} from './services/auth-services/my-auth.service';
-import {SharedModule} from './modules/shared/shared.module';
-import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MyAuthInterceptor } from './services/auth-services/my-auth-interceptor.service';
+import { MyAuthService } from './services/auth-services/my-auth.service';
+import { SharedModule } from './modules/shared/shared.module';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -19,36 +18,38 @@ import { MatIconModule } from '@angular/material/icon';
 
 import { FormsModule } from '@angular/forms';
 
+import {AuthRoutingModule} from './modules/auth/auth-routing.module';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserAnimationsModule, // Potrebno za animacije
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     SharedModule,
-    BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
     MatDatepickerModule,
     MatNativeDateModule,
     MatButtonModule,
     MatIconModule,
-    FormsModule
+    FormsModule,
+    AuthRoutingModule
+
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: MyAuthInterceptor,
-      multi: true // Ensures multiple interceptors can be used if needed
+      multi: true
     },
     MyAuthService,
-    provideAnimationsAsync() // Ensure MyAuthService is available for the interceptor
+    provideAnimations()
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
-
+export class AppModule { }
